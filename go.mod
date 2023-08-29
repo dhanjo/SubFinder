6e1b256150161cca422bb506c2989e6f9fdfe404
Dhananjay Garg <dhananjaygarg@Dhananjays-MacBook-Pro.local>
2023-08-30 01:49:34 +0530

    Implementing the subdfind.sh for enumerating the subdomains

---
+++ go.mod
@@ -0,0 +1,5 @@
+module scanner-layout
+
+go 1.19
+
+require github.com/tomnomnom/assetfinder v0.1.1 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"scanner-layout/SubEnum"
+)
+
+func main() {
+	fmt.Println("Hello")
+	SubEnum.SubEnum("vassagoconsultancy.com")
+}
